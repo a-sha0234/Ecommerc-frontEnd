9d2bb85101804cd0f0ffadb41e25dd612f92305a
a-sha0234 <asharma_31@hotmail.com>
2022-06-14 17:50:53 +0100

    create logic for different routes of the webpage and create api call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,67 @@
+import { react, useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { fetchData } from "./ApiCalls/getItems";
+//component imports
+
+import Navbar from "./components/Navbar";
+import Home from "./Pages/Home";
+import ItemDetails from "./components/ItemDetails";
+import Header from "./components/Header";
+import Traineritems from "./Pages/TrainerItems";
+import ShirtItems from "./Pages/ShirtItems";
+
 function App() {
-  return <div className="App"></div>;
+  const [itemData, setItemData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function getItemsbyType(type) {
+    let arr = itemData.items.filter((item) => {
+      if (item.ProductType == "shirt") {
+        return { item };
+      }
+    });
+
+    return arr;
+  }
+
+  useEffect(() => {
+    fetchData(setItemData);
+  }, []);
+
+  useEffect(() => {
+    if (itemData.length > 0) {
+      setIsLoading(false);
+    } else {
+      setIsLoading(true);
+    }
+  }, [itemData]);
+
+  return (
+    <BrowserRouter>
+      {" "}
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home items={itemData} />}></Route>
+        <Route
+          path="/trainers"
+          element={<Traineritems items={itemData} />}
+        ></Route>
+        <Route path="/shirts" element={<ShirtItems items={itemData} />}></Route>
+
+        {isLoading == false &&
+          itemData.map((data) => {
+            let route = `/item/${data._id}`;
+            return (
+              <Route
+                path={route}
+                element={<ItemDetails details={data} />}
+              ></Route>
+            );
+          })}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
